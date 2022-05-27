d967e9ce7b1fcf49a7d5161c38523f8972c4c984
Jalinson Diaz <zjaaaldev@gmail.com>
2022-05-26 21:35:04 -0300

    Now you can render different projects
    Events are not working on rendered projects

--- style-events.js
+++ style-events.js
@@ -1,43 +1,37 @@
-export const addEventListeners = () => {
-    const checkboxes = document.querySelectorAll(".checkmark");
 
-    const state ={
-        actualCheckbox:{},
-        actualLabel:{}
-    }
+const checkboxes = document.querySelectorAll(".checkmark");
 
-    const useState = (key, value) => state[key] = value;
-
-    const toggleCheck = (e) =>{
-        useState("actualCheckbox", e.target.previousElementSibling);
-        useState("actualLabel", e.target.parentElement);
-
-        state.actualCheckbox.checked = !state.actualCheckbox.checked;
-            if(state.actualCheckbox.checked){
-                e.target.classList.add("checked");
-                state.actualLabel.classList.add("checked");
-            
-            }else{
-                e.target.classList.remove("checked");
-                state.actualLabel.classList.remove("checked");
-            }
-
-    }
-    const hoverAction = (e) =>{
-        e.target.classList.add("hover");
-    }
-    const blurAction = (e) =>{
-        e.target.classList.remove("hover");
-    }
-    checkboxes.forEach(element =>{
-        element.addEventListener("click", toggleCheck);
-    })
-
-    checkboxes.forEach(element => {
-        element.addEventListener("mouseenter", hoverAction);
-    })
-    checkboxes.forEach(element => {
-        element.addEventListener("mouseout", blurAction);
-    })
+const state ={
+    actualCheckbox:{},
+    actualLabel:{}
 }
-addEventListeners();
+
+const useState = (key, value) => state[key] = value;
+
+const toggleCheck = (e) =>{
+    useState("actualCheckbox", e.target.previousElementSibling);
+    useState("actualLabel", e.target.nextElementSibling);
+
+    state.actualCheckbox.checked = !state.actualCheckbox.checked;
+
+        if(state.actualCheckbox.checked){
+            e.target.classList.add("checked");
+            state.actualLabel.classList.add("checked");
+        
+        }else{
+            e.target.classList.remove("checked");
+            state.actualLabel.classList.remove("checked");
+        }
+
+}
+const hoverAction = (e) =>{
+    e.target.classList.add("hover");
+}
+const blurAction = (e) =>{
+    e.target.classList.remove("hover");
+}
+checkboxes.forEach(element =>{
+    element.addEventListener("click", toggleCheck);
+    element.addEventListener("mouseout", blurAction);
+    element.addEventListener("mouseenter", hoverAction);
+})
