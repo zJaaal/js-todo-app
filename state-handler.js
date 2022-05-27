883a9772078ecfa1056df0398e2d6dbcccdd468b
Jalinson Diaz <zjaaaldev@gmail.com>
2022-05-26 23:35:21 -0300

    fixed format error

--- state-handler.js
+++ state-handler.js
@@ -1,59 +1,56 @@
+
 import { projects } from "./state";
 import { addStylesEventListener } from "./style-events";
-const renderCurrentProject = (e) => {
-  let index;
+const renderCurrentProject = (e) =>{
+    let index;
+    
+    if(!e){
+        index = "0";
+    }else
+        index = e.target.getAttribute("id");
 
-  if (!e) {
-    index = "0";
-  } else index = e.target.getAttribute("id");
+    const btnText = document.querySelector(".btn-text");
+    btnText.innerHTML = projects[index].name;
 
-  const btnText = document.querySelector(".btn-text");
-  btnText.innerHTML = projects[index].name;
+    const checkboxGroup = document.querySelector(".checkbox-group");
+    checkboxGroup.innerHTML = "";
 
-  const checkboxGroup = document.querySelector(".checkbox-group");
-  checkboxGroup.innerHTML = "";
+    let fragment = document.createDocumentFragment();
 
-  let fragment = document.createDocumentFragment();
+    projects[index].tasks.forEach( (task, i) =>{
+        fragment.appendChild(createGroupRow(task, i));
+    })
 
-  projects[index].tasks.forEach((task, i) => {
-    fragment.appendChild(createGroupRow(task, i));
-  });
+    checkboxGroup.appendChild(fragment);
+    addStylesEventListener();
+}
 
-  checkboxGroup.appendChild(fragment);
-  addStylesEventListener();
-};
-
-const createGroupRow = (task, index) => {
-  const div = document.createElement("div");
-  div.setAttribute(
-    "style",
-    "display: flex; flex-grow: 1; align-items: center; justify-content: space-between;"
-  );
-  div.innerHTML = `<div style="display: flex; flex: 1; flex-grow: 1; align-items: center;">
+const createGroupRow = (task, index) =>{
+        const div = document.createElement("div");
+        div.setAttribute("style", "display: flex; flex-grow: 1; align-items: center; justify-content: space-between;");
+        div.innerHTML = (`<div style="display: flex; flex: 1; flex-grow: 1; align-items: center;">
         <input type="checkbox" id="checkbox-${index}"/>
         <span class="checkmark"></span>
         <label for="" class="group-row">${task.toDo}</label>
       </div>
-      <img class="pencil" src="pencil.svg" alt="">`;
-
-  return div;
-};
-const createProject = (project) => {
-  projects.push({
-    name: project,
-    tasks: [
-      {
-        toDo: "You can edit me!",
-        isDone: false,
-      },
-    ],
-  });
-};
+      <img class="pencil" src="pencil.svg" alt="">`);     
+    
+    return div;
+}
+const createProject = (project) =>{
+    projects.push({
+        "name": project,
+        "tasks":[{
+            "toDo":"You can edit me!",
+            "isDone": false
+        }]
+    });
+} 
 
 const dropdownItems = document.querySelectorAll(".dropdown-item");
 
-dropdownItems.forEach((element) => {
-  element.addEventListener("click", renderCurrentProject);
-});
+dropdownItems.forEach(element =>{
+    element.addEventListener("click", renderCurrentProject);
+})
 
 renderCurrentProject();
