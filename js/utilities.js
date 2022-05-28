6f1e06afbf171dc1bc8eb3c78dd0b6f86dba3747
Jalinson Diaz <zjaaaldev@gmail.com>
2022-05-27 21:07:26 -0300

    Changed the vision of the project
    I realized that I was making spaghetti code so I just will make it simpler to do something that works. In the future ill make the full Project.

--- js/utilities.js
+++ js/utilities.js
@@ -1,23 +1,12 @@
- import { projects } from "../js/state.js";
- 
- export const createGroupRow = (task, index) =>{
+export const createGroupRow = (task, index) =>{
     const div = document.createElement("div");
     div.setAttribute("style", "display: flex; flex-grow: 1; align-items: center; justify-content: space-between;");
     div.innerHTML = (`<div style="display: flex; flex: 1; flex-grow: 1; align-items: center;">
     <input type="checkbox" id="checkbox-${index}"/>
     <span class="checkmark"></span>
-    <label for="checkbox-${index}" class="group-row">${task.toDo}</label>
+    <label for="checkbox-${index}" class="group-row">${task}</label>
   </div>
   <img class="pencil" src="pencil.svg" alt="">`);     
 
 return div;
 }
-export const createProject = (project) =>{
-projects.push({
-    "name": project,
-    "tasks":[{
-        "toDo":"You can edit me!",
-        "isDone": false
-    }]
-});
-} 
